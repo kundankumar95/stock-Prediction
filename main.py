ed06b9df9a941b9d7583374cac48155d5924f592
kundankumar95 <kundanburnwal32434@gmail.com>
2024-04-06 07:30:25 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import quandl, math, datetime
+import numpy as np
+from sklearn import preprocessing, model_selection
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import matplotlib.style as style
+
+style.use('ggplot')
+
+df = quandl.get('WIKI/GOOGL')
+df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100
+
+df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
+
+forecast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+
+forecast_out = int(math.ceil(0.1*len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+
+X = np.array(df.drop(['label'], axis=1))
+X = preprocessing.scale(X)
+X_laterly = X[-forecast_out:]
+X = X[:-forecast_out]
+y = np.array(df['label'])
+y = y[:-forecast_out] 
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+acc = clf.score(X_test, y_test)
+print(acc)
+
+forecast_set = clf.predict(X_laterly)
+print(forecast_set, acc, forecast_out)
+
+df['Forecast'] = np.nan
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in  forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range (len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.title('Adjusted Close Price')
+plt.show()
